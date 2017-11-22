d1b3313f1d979a771d97914d01f2cd8903c27b46
Mona subaih <m.subaih@rozn.org>
2017-11-22 04:59:59 +0200

    g

--- index.js
+++ index.js
@@ -1,63 +1,58 @@
 'use strict';
 
 // Imports dependencies and set up http server
-const 
+const
   express = require('express'),
   bodyParser = require('body-parser'),
   app = express().use(bodyParser.json()); // creates express http server
 
-// Sets server port and logs message on success
-app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+app.post('/webhook', function (req, res) {
+  var data = req.body;
+  // Make sure this is a page subscription
+  if (data.object == 'page') {
+    // Iterate over each entry
+    data.entry.forEach(function(pageEntry) {
+//Newsfeed changes webhook request
 
-// Creates the endpoint for our webhook 
-app.post('/webhook', (req, res) => {  
- 
-  let body = req.body;
+if(pageEntry.hasOwnProperty('changes')){
+        pageEntry.changes.forEach(function(changes){
+          if(changes.field=="feed" && changes.value.item=="comment" && changes.value.verb=="add"){
+            var messageData = {
+ message: "hello"
+              };
 
-  // Checks this is an event from a page subscription
-  if (body.object === 'page') {
+callPrivateReply(messageData,changes.value.comment_id);
+          }
+        });
+      }
+//Messenger webhook request
 
-    // Iterates over each entry - there may be multiple if batched
-    body.entry.forEach(function(entry) {
-
-      // Gets the message. entry.messaging is an array, but 
-      // will only ever contain one message, so we get index 0
-      let webhookEvent = entry.messaging[0];
-      console.log(webhookEvent);
+      if(pageEntry.hasOwnProperty('messaging')){
+        //messenger code goes here
+      }
     });
 
-    // Returns a '200 OK' response to all requests
-    res.status(200).send('EVENT_RECEIVED');
-  } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
+// Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know you've
+    // successfully received the callback. Otherwise, the request will time out.
+    res.sendStatus(200);
   }
-
 });
-// Adds support for GET requests to our webhook
-app.get('/webhook', (req, res) => {
 
-  // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = "EAACEdEose0cBADA7gR84fou2KqfRYD6sPTMK983I75kbir8gZB30kMJp8YpRCqMGDBsJpzWsoD0Jju3UXLGC21ZC5wY3LNH9cXFVK3NG8NqsBYx61nNqw5OLa1LZAhIIc2BeX3u6VzpZAY5DqvZBoLn7DOUZBakgs6p1xtcCfdN3MZC0Ugr3fnjoZAkncHeBevQZD";
-    
-  // Parse the query params
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
-    
-  // Checks if a token and mode is in the query string of the request
-  if (mode && token) {
-  
-    // Checks the mode and token sent is correct
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      
-      // Responds with the challenge token from the request
-      console.log('WEBHOOK_VERIFIED');
-      res.status(200).send(challenge);
-    
+
+function callPrivateReply(messageData,comment_id) {
+  request({
+    uri: 'https://graph.facebook.com/v2.9/'+comment_id+'/private_replies',
+    qs: { access_token: "EAACEdEose0cBAM6PDGqLzdYxKIIhKqW2j65DOBfyyZCUDoMAzZBSdRR9N7ToOkEpDji1ZCTUAndz1M2yiouS205OAIXYfWS5FZAOWLz9WeahN2PgomgUz22hSCzP2DwyVSsmJgsrrsmMtrH8C3K6LRnPkvZANI4tXIKC3vItvpzPbEtummJPCDYFqhl937pFRFZAEzVUXqBQZDZD" },
+    method: 'POST',
+    json: messageData
+
+}, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log(body);
     } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);      
-    }
-  }
-});
+      console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
+ }
+  });
+}
