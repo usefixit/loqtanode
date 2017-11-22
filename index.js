f40e1df7ff2840d2a2f0fe0216c9416078cae1cd
Mona subaih <m.subaih@rozn.org>
2017-11-22 15:20:59 +0200

    sss

--- index.js
+++ index.js
@@ -43,7 +43,22 @@ app.get('/webhook', (req, res) => {
     var messageData = {
         message: "اثممخ"
       };
-callCommentReply(messageData,"650177341837555_719477614907527");
+
+request({
+  uri: 'https://graph.facebook.com/v2.11/650177341837555_719477614907527/comments',
+  qs: { access_token: "EAAY9O9xDRkYBACkqBn52XD75gbZCNbTj0BempuU0NnHmLlZC8GujSyiswlw5jN3OD1IDHylZASjzZBsL4lk1Q5kPOZAezhw32z2mYovwzSBnRFGem3Sb1clFWqPSD5F9UbWdOcpfiK0TTBuQ8MdhCSdMuwd6mqMf0qkGbfyZAKUAZDZD" },
+  method: 'POST',
+  json: messageData
+
+}, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log(body);
+  } else {
+    console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
+  }
+});
+
+
     // Checks this is an event from a page subscription
     if (body.object === 'page') {
 
@@ -61,7 +76,21 @@ callCommentReply(messageData,"650177341837555_719477614907527");
                     message: "hello auto cmmenter"
                   };
 
-    callCommentReply(messageData,changes.value.comment_id);
+    request({
+      uri: 'https://graph.facebook.com/v2.11/'+changes.value.comment_id+'/comments',
+      qs: { access_token: "EAAY9O9xDRkYBACkqBn52XD75gbZCNbTj0BempuU0NnHmLlZC8GujSyiswlw5jN3OD1IDHylZASjzZBsL4lk1Q5kPOZAezhw32z2mYovwzSBnRFGem3Sb1clFWqPSD5F9UbWdOcpfiK0TTBuQ8MdhCSdMuwd6mqMf0qkGbfyZAKUAZDZD" },
+      method: 'POST',
+      json: messageData
+
+  }, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        console.log(body);
+      } else {
+        console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
+      }
+    });
+
+
               }
             });
           }
@@ -77,18 +106,3 @@ callCommentReply(messageData,"650177341837555_719477614907527");
     }
 
   });
-  function callCommentReply(messageData,comment_id) {
-  request({
-    uri: 'https://graph.facebook.com/v2.11/'+comment_id+'/comments',
-    qs: { access_token: "EAAY9O9xDRkYBACkqBn52XD75gbZCNbTj0BempuU0NnHmLlZC8GujSyiswlw5jN3OD1IDHylZASjzZBsL4lk1Q5kPOZAezhw32z2mYovwzSBnRFGem3Sb1clFWqPSD5F9UbWdOcpfiK0TTBuQ8MdhCSdMuwd6mqMf0qkGbfyZAKUAZDZD" },
-    method: 'POST',
-    json: messageData
-
-}, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      console.log(body);
-    } else {
-      console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
-    }
-  });
-}
