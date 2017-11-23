0ebb93bc46cc58c82d99997c29f14178fbf54c1d
Mona subaih <m.subaih@rozn.org>
2017-11-23 10:00:07 +0200

    dd

--- index.js
+++ index.js
@@ -40,25 +40,7 @@ app.get('/webhook', (req, res) => {
   app.post('/webhook', (req, res) => {
 
     let body = req.body;
-    var messageData = {
-        message: "اثممخ"
-      };
 
-request({
-  uri: 'https://graph.facebook.com/v2.11/650177341837555_719477614907527/comments',
-  qs: { access_token: "EAAY9O9xDRkYBACkqBn52XD75gbZCNbTj0BempuU0NnHmLlZC8GujSyiswlw5jN3OD1IDHylZASjzZBsL4lk1Q5kPOZAezhw32z2mYovwzSBnRFGem3Sb1clFWqPSD5F9UbWdOcpfiK0TTBuQ8MdhCSdMuwd6mqMf0qkGbfyZAKUAZDZD" },
-  method: 'POST',
-  json: messageData
-
-}, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body);
-  } else {
-    console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
-  }
-});
-
-/*
     // Checks this is an event from a page subscription
     if (body.object === 'page') {
 
@@ -74,7 +56,7 @@ request({
                   };
 
     request({
-      uri: 'https://graph.facebook.com/v2.11/'+changes.value.comment_id+'/comments',
+      uri: 'https://graph.facebook.com/v2.11/'+changes.value.post_id+'_'+changes.value.comment_id+'/comments',
       qs: { access_token: "EAAY9O9xDRkYBACkqBn52XD75gbZCNbTj0BempuU0NnHmLlZC8GujSyiswlw5jN3OD1IDHylZASjzZBsL4lk1Q5kPOZAezhw32z2mYovwzSBnRFGem3Sb1clFWqPSD5F9UbWdOcpfiK0TTBuQ8MdhCSdMuwd6mqMf0qkGbfyZAKUAZDZD" },
       method: 'POST',
       json: messageData
@@ -87,7 +69,6 @@ request({
       }
     });
 
-
               }
             });
           }
@@ -96,11 +77,9 @@ request({
 
       // Returns a '200 OK' response to all requests
       res.status(200).send('EVENT_RECEIVED');
-    } else {
-      // Returns a '404 Not Found' if event is not from a page subscription
-      res.sendStatus(404);
     }
-*/
+
 res.status(200).send('EVENT_RECEIVED');
 
   });
+  app.listen();
