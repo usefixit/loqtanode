52ef394088d91284d21db0768cf4276d7a4a0e16
Mona subaih <m.subaih@rozn.org>
2017-11-25 06:37:41 +0200

    uu

--- index.js
+++ index.js
@@ -6,12 +6,16 @@ const
   bodyParser = require('body-parser'),
   request = require('request'),
   getUrls = require("get-urls"),
+  striptags = require('striptags'),
   app = express().use(bodyParser.json()); // creates express http server
 
   //some global variable
   let   content = '';
   let firstLink = '';
   let productSerach = '';
+  let smallDescriptionOpenTag = "[smallDescription]",
+      smallDesc = '',
+      smallDescriptionCloseTag = "[/smallDescription]";
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
@@ -74,19 +78,36 @@ app.get('/webhook', (req, res) => {
                    var messageData = {
                        message: "احنا متجر الكتروني بغزة وطلبك بيوصل لعندك ،السعر رح تلاقي بالرابط\n " + firstLink
                      };
-                   request({
-                     uri: 'https://graph.facebook.com/v2.11/'+changes.value.comment_id+'/comments',
-                     qs: { access_token: "EAAY9O9xDRkYBACkqBn52XD75gbZCNbTj0BempuU0NnHmLlZC8GujSyiswlw5jN3OD1IDHylZASjzZBsL4lk1Q5kPOZAezhw32z2mYovwzSBnRFGem3Sb1clFWqPSD5F9UbWdOcpfiK0TTBuQ8MdhCSdMuwd6mqMf0qkGbfyZAKUAZDZD" },
-                     method: 'POST',
-                     json: messageData
 
-                 }, function (error, response, body) {
-                     if (!error && response.statusCode == 200) {
-                       console.log(body);
-                     } else {
-                       console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
-                     }
-                   });
+                   callPrivateReplyorComment(messageData,changes.value.comment_id,"comments");
+
+//////////////////send private messageData and get short description
+                request({
+                  uri: ''+productSerach,
+                  method: 'GET'
+                }, function (error, response, body) {
+                  if (!error && response.statusCode == 200) {
+                       let productinfo = JSON.parse(body);
+                       if(productinfo.hasOwnProperty('products')){
+                       if(productinfo.products.length > 0){
+                        let description =  striptags(productinfo.products[0].body_html);
+                        let smallDescStartIndex = description.indexOf(smallDescriptionOpenTag);
+                        let smallDescEndIndex = description.indexOf(smallDescriptionCloseTag);
+                        smallDesc = description.substring(smallDescStartIndex, smallDescEndIndex).replace(/(\[.+\])/gi, '').trim();
+
+                        var messageData = {
+                        message: smallDesc + "\n"+"اطلبه الان من الرابط"+"\n"+firstLink
+                        };
+                //send private message
+                callPrivateReplyorComment(messageData,changes.value.comment_id,"private_replies");
+
+                }}
+                  } else {
+                    console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
+                  }
+                });
+                ///////////////////////////////
+
                    ///////////////////////////
                   } else {
                     console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
@@ -106,3 +127,19 @@ app.get('/webhook', (req, res) => {
 res.status(200).send('EVENT_RECEIVED');
 
   });
+
+  function callPrivateReplyorComment(messageData,comment_id,action) {
+    request({
+      uri: 'https://graph.facebook.com/v2.9/'+comment_id+'/'+action,
+      qs: { access_token: "EAAY9O9xDRkYBACkqBn52XD75gbZCNbTj0BempuU0NnHmLlZC8GujSyiswlw5jN3OD1IDHylZASjzZBsL4lk1Q5kPOZAezhw32z2mYovwzSBnRFGem3Sb1clFWqPSD5F9UbWdOcpfiK0TTBuQ8MdhCSdMuwd6mqMf0qkGbfyZAKUAZDZD" },
+      method: 'POST',
+      json: messageData
+
+  }, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        console.log(body);
+      } else {
+        console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
+      }
+    });
+  }
